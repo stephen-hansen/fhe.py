389dadc942b898bba373ac9c6a28deddb342a21c
Stephen Hansen <sph77@drexel.edu>
2021-11-16 01:24:17 -0500

    scheme updates, doesnt work

--- dghv.py
+++ dghv.py
@@ -1,6 +1,10 @@
+import math
 import random
 from abc import ABC, abstractmethod
 from scheme import *
+
+def lsb(c):
+    return c & 1
 
 def toBitOrder(number, precision=None):
     # Bit order is little endian (LSB first)
@@ -17,13 +21,50 @@ class DGHV(HomomorphicEncryptionScheme):
         # Key is random P-bit odd integer
         self.secretKey = random.randrange(2 ** (self.P - 1) + 1, 2 ** self.P, 2)
         self.publicKey = [self.encryptWithSecret(self.secretKey, 0) for _ in range(lmbda)]
-        return self.publicKey, self.secretKey
+        # Generate the random subset
+        target = 1/self.secretKey
+        itemset = []
+        self.alpha = 0
+        while target > 0:
+            randvalue = random.uniform(0, 2)
+            while randvalue == 2:
+                randvalue = random.uniform(0, 2)
+            if target < randvalue:
+                randvalue = target
+            target -= randvalue
+            self.alpha += 1
+            itemset.append([randvalue, 1])
+            for _ in range(random.randint(1,4)):
+                randvalue = random.uniform(0, 2)
+                while randvalue == 2:
+                    randvalue = random.uniform(0, 2)
+                itemset.append([randvalue, 0])
+        random.shuffle(itemset)
+        ys = [x[0] for x in itemset]
+        self.beta = len(ys)
+        self.publicKeyStar = [self.publicKey, ys]
+        self.secretKeyStar = [x[1] for x in itemset]
+        self.encryptedSecretKey = [self.encrypt(self.publicKeyStar, b) for b in self.secretKeyStar]
+        return self.publicKeyStar, self.secretKeyStar
 
-    def encrypt(self, pubKey, m):
+    def origEncrypt(self, pubKey, m):
         subset = random.sample(pubKey, random.randint(1, len(pubKey)-1))
-        # In theory this is still 0 mod p
         subsetSum = sum(subset)
         return m + subsetSum
+
+    def encrypt(self, pubKey, m):
+        cipher = self.origEncrypt(pubKey[0], m)
+        zs = self.postprocess(cipher)
+        subsetsum = sum([self.secretKeyStar[i]*zs[i] for i in range(self.beta)])
+        return [cipher, zs]
+
+    def postprocess(self, cipher):
+        zs = []
+        for i in range(self.beta):
+            z = cipher * (self.publicKeyStar[1][i] % 2)
+            # Only keep log alpha bits (TODO)
+            zs.append(z)
+        return zs
 
     def encryptWithSecret(self, p, m):
         # m' is random N-bit number such that m' = m mod 2
@@ -44,14 +85,18 @@ class DGHV(HomomorphicEncryptionScheme):
             ciphers.append(self.encrypt(p, m))
         return ciphers
 
-    def decrypt(self, p, c):
+    def decrypt(self, sk, c):
         # c' is (c mod p) in (-p/2, p/2)
-        c_ = c % p # c_ in range [0, p)
+        #c_ = c % p # c_ in range [0, p)
         #if c_ >= p//2:
         #    # Range from [p/2, p) goes to [-p/2, 0)
         #    # Union'd with range [0, p/2) gives correct range [-p/2, p/2).
         #    c_ -= p
-        return c_ % 2
+        #return c_ % 2
+        zs = c[1]
+        subsetsum = sum([sk[i]*zs[i] for i in range(self.beta)])
+        roundedsum = round(subsetsum)
+        return lsb(c[0]) ^ lsb(roundedsum)
 
     def decryptNumber(self, p, cs):
         # Ciphers are little endian
@@ -64,17 +109,26 @@ class DGHV(HomomorphicEncryptionScheme):
         return result
 
     def add(self, c1, c2):
-        return c1 + c2
+        result = c1[0] + c2[0]
+        zs = self.postprocess(result)
+        return [result, zs]
 
     def sub(self, c1, c2):
-        return c1 - c2
+        result = c1[0] - c2[0]
+        zs = self.postprocess(result)
+        return [result, zs]
 
     def mult(self, c1, c2):
-        return c1 * c2
+        result = c1[0] * c2[0]
+        zs = self.postprocess(result)
+        return [result, zs]
 
     def evaluate(self, f, *cs):
         # Call f, pass in the args and the scheme
         return f(self, *cs)
+
+    def runRecrypt(self, c1):
+        return self.recrypt(self.publicKeyStar, decryptCircuit, self.encryptedSecretKey, c1)
 
     def recrypt(self, pk, D, sk, c1):
         # pk is the new public key to encrypt into
@@ -83,8 +137,8 @@ class DGHV(HomomorphicEncryptionScheme):
         # c1 is the ciphertext
         bits = toBitOrder(c1) # Get bits of c1 for encrypt
         c1_ = [self.encrypt(pk, b) for b in bits]
-        args = sk + c1_
-        c = self.evaluate(pk, D, *args)
+        args = [sk, c1_]
+        c = self.evaluate(pk, D, args)
         return c
 
 class Gate(ABC):
@@ -113,7 +167,7 @@ class NANDGate(Gate):
         andGate = ANDGate()
         andResult = andGate.run(scheme, inputs[0], inputs[1])
         xorGate = XORGate()
-        encrypted1 = scheme.encrypt(scheme.publicKey, 1)
+        encrypted1 = scheme.encrypt(scheme.publicKeyStar, 1)
         return xorGate.run(scheme, encrypted1, andResult)
 
 class FullAdder(Gate):
@@ -139,21 +193,15 @@ def addernbit(scheme, n, *ciphers):
         a.append(ciphers[i])
         b.append(ciphers[i+n])
     adder = FullAdder()
-    cIn = scheme.encrypt(scheme.publicKey, 0)
+    cIn = scheme.encrypt(scheme.publicKeyStar, 0)
     r = []
     for i in range(n):
         ri, cIn = adder.run(scheme, a[i], b[i], cIn)
         r.append(ri)
     return r
 
-def lsb(c):
-    return c & 1
-
 def decryptCircuit(scheme, *ciphers):
-    c = ciphers[0]
-    xorGate = XORGate()
-    m = xorGate.run(scheme, lsb(c), lsb(round(c/scheme.secretKey)))
-    return m
+    pass
 
 if __name__ == "__main__":
     # Some tests
@@ -173,6 +221,7 @@ if __name__ == "__main__":
             expected = bit1 ^ bit2
             encrypted1 = scheme.encrypt(publicKey, bit1)
             encrypted2 = scheme.encrypt(publicKey, bit2)
+            print(f"enc1={encrypted1},enc2={encrypted2}")
             encryptedActual = scheme.add(encrypted1, encrypted2)
             actual = scheme.decrypt(secretKey, encryptedActual)
             print(f"{bit1} ^ {bit2} = {expected} | {actual}")
@@ -180,6 +229,7 @@ if __name__ == "__main__":
             expected = bit1 & bit2
             encrypted1 = scheme.encrypt(publicKey, bit1)
             encrypted2 = scheme.encrypt(publicKey, bit2)
+            print(f"enc1={encrypted1},enc2={encrypted2}")
             encryptedActual = scheme.mult(encrypted1, encrypted2)
             actual = scheme.decrypt(secretKey, encryptedActual)
             print(f"{bit1} & {bit2} = {expected} | {actual}")
