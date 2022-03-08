0053419b6efc3d159a7a89836e914d9d3775f203
stephen-hansen <sph77@drexel.edu>
2022-03-08 16:24:55 -0500

    Working on circuit

--- dghv.py
+++ dghv.py
@@ -175,10 +175,10 @@ class DecryptCircuit():
         # TODO compute this under encrypted setting
         cy = c[1]
         Theta = math.ceil(math.log2(self.scheme.alpha))+3
+        """
         cy_enc = [toBitOrderFloat(cyi, Theta) for cyi in cy]
         cy_enc = [[self.scheme.encrypt(b) for b in cyi] for cyi in cy_enc]
         products = []
-        """
         for i, s in enumerate(self.encryptedkey):
             # Bit i goes to cy i
             # Mask each bit of each cy
@@ -190,8 +190,44 @@ class DecryptCircuit():
         for i in range(Theta):
             bits_i = [prod[i] for prod in products]
             W.append(self.hamming_weight(bits_i))
-        """
+        # Shift weights back
+        ws = []
+        for i, weight in enumerate(W):
+            w_i = []
+            for k in range(i):
+                w_i.append(self.scheme.encrypt(0))
+            for weight_i in weight:
+                w_i.append(weight_i)
+            for k in range(len(W) - 1 - i):
+                w_i.append(self.scheme.encrypt(0))
+            ws.append(w_i)
         # TODO two for three trick to add hamming weights
+        three_for_two_s = []
+        three_for_two_c = []
+        for i in range(len(ws[0])):
+            three_for_two_s.append(self.scheme.encrypt(0))
+            three_for_two_c.append(self.scheme.encrypt(0))
+        offset = 0
+        for w in ws:
+            # s, c extend by 1 per loop
+            next_s = [self.scheme.encrypt(0)]
+            next_c = []
+            new_w = []
+            for k in range(offset):
+                new_w.append(self.scheme.encrypt(0))
+            for w_i in w:
+                new_w.append(w_i)
+            for i, w_i in enumerate(new_w):
+                next_s.append(self.scheme.add(w_i, self.scheme.add(three_for_two_s[i], three_for_two_c[i]))) 
+                next_c.append(self.scheme.add(self.scheme.mult(w_i, three_for_two_s[i]),
+                    self.scheme.add(self.scheme.mult(three_for_two_s[i], three_for_two_c[i]),
+                        self.scheme.mult(three_for_two_c[i], w_i))))
+            next_c.append(self.scheme.encrypt(0))
+            three_for_two_s = next_s
+            three_for_two_c = next_c
+            offset+=1
+        """
+        # TODO carry propagate adder
         x = sum([cy[i] if v > 0 else 0 for i, v in enumerate(self.scheme.newsecret)])
         xbits = toBitOrderFloat(x % 2, Theta)
         rounded_sum = self.scheme.add(self.scheme.encrypt(xbits[0]), self.scheme.encrypt(xbits[1]))
